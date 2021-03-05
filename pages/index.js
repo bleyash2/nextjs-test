f737f08dfe929eaf60515a905752f0686a0b95f0
bleyash2 <29970532+bleyash2@users.noreply.github.com>
2021-03-05 16:41:18 +1100

    Adds Projects Files

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+// The Storyblok Client
+import Storyblok from "../lib/storyblok";
+import useStoryblok from "../lib/storyblok-hook";
+import DynamicComponent from "../components/DynamicComponent";
+
+export default function Home(props) {
+  // the Storyblok hook to enable live updates
+  const story = useStoryblok(props.story);
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <h1>{story ? story.name : "My Site"}</h1>
+      </header>
+
+      <main>
+        {story
+          ? story.content.body.map((blok) => <DynamicComponent blok={blok} key={blok._uid} />)
+          : null}
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  let slug = "home";
+  let params = {
+    version: "draft", // or 'published'
+  };
+
+  if (context.preview) {
+    params.version = "draft";
+    params.cv = Date.now();
+  }
+
+  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      preview: context.preview || false,
+    },
+    revalidate: 10,
+  };
+}
